05a319719a9fad9e7b10083fefebdf2f84f0eb7f
SpaceClouds42 <spaceclouds42@gmail.com>
2021-08-07 17:13:53 -0400

    add entry card generation, sodium as an example

---
+++ summer21/entries.js
@@ -0,0 +1,20 @@
+export default
+{
+  "mods": {
+    "sodium": {
+      "modrinthId": "AANobbMI",
+      "curseSlug": "sodium",
+      "repo": "jellysquid3/sodium-fabric",
+      "authors": {
+        "jellysquid": "jellysquid3"
+      }
+    }
+  },
+
+  "awards": {
+    "testAward": {
+      "displayName": "Test Award",
+      "awardees": [ "foo", "bar" ]
+    }
+  }
+}
