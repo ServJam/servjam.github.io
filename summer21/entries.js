9bd9ffa4fbffa32673e8ce96a56c0f8d691727a0
SpaceClouds42 <spaceclouds42@gmail.com>
2021-08-09 20:09:39 -0400

    finish entry/award page

--- summer21/entries.js
+++ summer21/entries.js
@@ -1,20 +1,21 @@
 export default
 {
   "mods": {
-    "sodium": {
+  /*"example": {
       "modrinthId": "AANobbMI",
       "curseSlug": "sodium",
       "repo": "jellysquid3/sodium-fabric",
       "authors": {
+        Display Name: GitHub Username
         "jellysquid": "jellysquid3"
       }
-    }
+    }*/
   },
 
-  "awards": {
-    "testAward": {
-      "displayName": "Test Award",
-      "awardees": [ "foo", "bar" ]
-    }
+  "awards": { /*
+    "First to Complete": {
+      "description": "The first mod to be completed during the ServJam Summer 2021 event.",
+      "awardees": [ ]
+    } */
   }
 }
